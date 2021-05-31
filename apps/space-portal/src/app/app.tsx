2bb19f84b7b978e047a1197c24b668879583d150
Xuan Phong <xuanphong3107@gmail.com>
2021-06-01 01:25:02 +0300

    initial commit

--- apps/space-portal/src/app/app.tsx
+++ apps/space-portal/src/app/app.tsx
@@ -1,99 +1,177 @@
-import styles from './app.module.css';
-
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
+import { useState, useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <div className={styles.app}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to space-portal!</h1>
-      </header>
-      <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <img src={star} className="material-icons" alt="" />
-              Star
-            </div>
-          </a>
-        </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
-            >
-              Scale React Development with Nx (Course)
+    <div>
+      <header>
+        <a href="#" className="logo">
+          PORTAL
+        </a>
+        <ul>
+          <li>
+            <a href="#" className="active">
+              Homeland
             </a>
           </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/latest/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
+          <li>
+            <a href="#">Artifact</a>
           </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://nx.app/">
-              <svg
-                width="36"
-                height="36"
-                viewBox="0 0 120 120"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z"
-                  fill="#0E2039"
-                />
-                <path
-                  d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z"
-                  fill="white"
-                />
-              </svg>
-              <span className="gutter-left">Nx Cloud</span>
-            </a>
+          <li>
+            <a href="#">Tech</a>
+          </li>
+          <li>
+            <a href="#">E.T</a>
           </li>
         </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-  `}</pre>
-        </details>
-      </main>
+      </header>
+      <section>
+        <img src="../assets/mesh.png" id="mesh" />
+        <img
+          src="../assets/star.png"
+          id="star"
+          style={{ left: offsetY * 0.25 + 'px' }}
+        />
+        <img
+          src="../assets/mountain.png"
+          id="mountain"
+          style={{ top: offsetY * 0.2 + 'px' }}
+        />
+        <img src="../assets/portal.png" id="portal" />
+        <a href="#info" id="btn" style={{ marginTop: offsetY * 1.5 + 'px' }}>
+          Teleport
+        </a>
+        <img
+          src="../assets/rock.png"
+          id="rock"
+          style={{ top: offsetY * 0.3 + 'px' }}
+        />
+      </section>
+      <div className="info" id="info">
+        <h2 data-aos="fade-up">Enter The World Of Parallax Effects</h2>
+        <p data-aos="fade-up">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit
+          doloribus tenetur fugiat, temporibus enim commodi iusto libero magni
+          deleniti quod quam consequuntur! Commodi minima excepturi repudiandae
+          velit hic maxime doloremque. Quaerat provident commodi consectetur
+          veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+          pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+          excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+          Voluptatem quaerat non architecto ab laudantium modi minima sunt esse
+          temporibus sint culpa, recusandae aliquam numquam totam ratione
+          voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
+          aliquam eligendi, placeat qui corporis!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit
+          doloribus tenetur fugiat, temporibus enim commodi iusto libero magni
+          deleniti quod quam consequuntur! Commodi minima excepturi repudiandae
+          velit hic maxime doloremque. Quaerat provident commodi consectetur
+          veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+          pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+          excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+          Voluptatem quaerat non architecto ab laudantium modi minima sunt esse
+          temporibus sint culpa, recusandae aliquam numquam totam ratione
+          voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
+          aliquam eligendi, placeat qui corporis!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit
+          doloribus tenetur fugiat, temporibus enim commodi iusto libero magni
+          deleniti quod quam consequuntur! Commodi minima excepturi repudiandae
+          velit hic maxime doloremque. Quaerat provident commodi consectetur
+          veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+          pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+          excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+          Voluptatem quaerat non architecto ab laudantium modi minima sunt esse
+          temporibus sint culpa, recusandae aliquam numquam totam ratione
+          voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
+          aliquam eligendi, placeat qui corporis!
+        </p>
+        <div className="article">
+          <div className="des">
+            <h2 data-aos="fade-left">Lorem ipsum</h2>
+            <p data-aos="fade-left">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+              fugiat iusto fuga praesentium optio, eaque rerum! Provident
+              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+              eius earum ut molestias architecto voluptate aliquam nihil,
+              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+              tenetur error, harum nesciunt ipsum debitis quas aliquid.
+              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
+              laudantium molestias eos sapiente officiis modi at sunt excepturi
+              expedita sint? Sed quibusdam recusandae alias error harum maxime
+              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+              Officiis iure rerum voluptates a cumque velit quibusdam sed amet
+              tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat,
+              temporibus enim commodi iusto libero magni deleniti quod quam
+              consequuntur! Commodi minima excepturi repudiandae velit hic
+              maxime doloremque. Quaerat provident commodi consectetur veniam
+              similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+              pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+              excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+              Voluptatem quaerat non architecto ab laudantium modi minima sunt
+              esse temporibus sint culpa, recusandae aliquam numquam totam
+              ratione voluptas quod exercitationem fuga. Possimus quis earum
+              veniam quasi aliquam eligendi, placeat qui corporis!
+            </p>
+          </div>
+          <img
+            data-aos="fade-left"
+            className="article-img"
+            src="../assets/black-hole.svg"
+            id="black-hole"
+          />
+        </div>
+      </div>
     </div>
   );
 }
